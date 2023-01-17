6285a515ce5f9ddfdb7c44176060dd2b41de46f9
Nurdin Djumabekov <dui.nurdin@gmail.com>
2023-01-17 18:57:26 +0600

    Add files via upload

--- DZ3_Nurdin_20-03/js/app.js
+++ DZ3_Nurdin_20-03/js/app.js
@@ -3,50 +3,54 @@ const tabParent = document.querySelector(".tabheader__items");
 const tabItems = document.querySelectorAll(".tabheader__item");
 const tabContent = document.querySelectorAll(".tabcontent");
 
-const hideContent = () => {
-  tabContent.forEach((i) => {
-    i.style.display = "none";
+const hideTabContent = () => {
+  tabContent.forEach((tab) => {
+    tab.style.display = "none";
   });
-  tabItems.forEach((j) => {
-    j.classList.remove("tabheader__item_active");
+  tabItems.forEach((i) => {
+    i.classList.remove("tabheader__item_active");
   });
 };
+hideTabContent();
 
-hideContent();
+//////////////////////////////////////////////////////////////////
 
-const showContent = (n = 0) => {
-  tabContent[n].style.display = "block";
-  tabItems[n].classList.add("tabheader__item_active");
+
+const showContent = (i = 0) => {
+  tabContent[i].style.display = "block";
+  tabItems[i].classList.add("tabheader__item_active");
 };
-
 showContent();
-
-//////////////////
-
-tabParent.addEventListener("click", (e) => {
-  const target = e.target;
+tabParent.addEventListener("click", (event) => {
+  const target = event.target;
+  // если не содержит!!
   if (!target.classList.contains("tabheader__item_active")) {
     tabItems.forEach((item, index) => {
       if (item === target) {
-        hideContent();
+        hideTabContent();
         showContent(index);
+        //////////////////////////////
+        // console.log(index);
+        // console.log(item);
       }
     });
-    if (condition) {
-    }
   }
 });
 
-///////////////modal/////////////////
+//////////////////////////////////////////////////////////////////
+
 
 const modal = document.querySelector(".modal");
 const open = document.querySelector("#open_modal");
 const close = document.querySelector("#close_modal");
-
 open.addEventListener("click", () => {
-  modal.style.display = "block";
+  // modal.style.display = "block";
+  modal.classList.remove("close_modal");
+  modal.classList.add("open_modal");
 });
 
 close.addEventListener("click", () => {
-  modal.style.display = "none";
+  // modal.style.display = "none";
+  modal.classList.remove("open_modal");
+  modal.classList.add("close_modal");
 });
